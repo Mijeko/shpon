f8b908f748b4264f18941ae6a08afe0666019359
small_jedi <popugau@gmail.com>
2023-06-18 01:31:31 +0700

    update

--- markup/src/js/include/sliders.js
+++ markup/src/js/include/sliders.js
@@ -1,12 +1,70 @@
 import Swiper, {Navigation, Pagination} from 'swiper';
 
-let blockInfo;
+let blockInfo, bigSlider;
+
+const fraction = document.getElementById("pagination-control-fraction");
 
 initInfoBlockSlider();
+initBigSlider();
 
 window.addEventListener('resize', function (event) {
     initInfoBlockSlider();
+    initBigSlider();
 }, true);
+
+function initBigSlider() {
+
+    if (document.querySelectorAll(".big-slider").length <= 0) {
+        return false;
+    }
+
+    if (bigSlider && bigSlider.destroy && typeof bigSlider.destroy === 'function') {
+        bigSlider.destroy(false);
+    }
+
+    if (windowWidth() > 990) {
+        bigSlider = new Swiper(".big-slider", {
+            modules: [Navigation, Pagination],
+            navigation: {
+                nextEl: ".swiper-button-next",
+                prevEl: ".swiper-button-prev",
+            },
+            pagination: {
+                el: ".custom-slider-pagination",
+                type: "bullets",
+            },
+            on: {
+                init: (swiper) => {
+                    fraction.innerHTML = renderFraction(swiper);
+                },
+                slideChange: (swiper) => {
+                    fraction.innerHTML = renderFraction(swiper);
+                }
+            }
+        });
+    } else {
+        bigSlider = new Swiper(".big-slider", {
+            modules: [Navigation, Pagination],
+            navigation: {
+                nextEl: ".swiper-button-next",
+                prevEl: ".swiper-button-prev",
+            },
+            pagination: {
+                el: ".big-slider-mobile-progress-pagination",
+                type: "progressbar",
+            },
+            on: {
+                init: (swiper) => {
+                    fraction.innerHTML = renderFraction(swiper);
+                },
+                slideChange: (swiper) => {
+                    fraction.innerHTML = renderFraction(swiper);
+                }
+            }
+        });
+    }
+
+}
 
 function initInfoBlockSlider() {
     if (document.querySelectorAll(".block-info").length <= 0) {
@@ -30,38 +88,10 @@ function windowWidth() {
     return window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
 }
 
-const fraction = document.getElementById("pagination-control-fraction");
-new Swiper(".big-slider", {
-    modules: [Navigation, Pagination],
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-    },
-    pagination: {
-        el: ".big-slider-mobile-progress-pagination",
-        type: "progressbar",
-    },
-    breakpoints: {
-        640: {
-            // pagination: {
-            //     el: ".swiper-pagination",
-            // },
-        }
-    },
-
-    on: {
-        init: (swiper) => {
-            fraction.innerHTML = renderFraction(swiper);
-        },
-        slideChange: (swiper) => {
-            fraction.innerHTML = renderFraction(swiper);
-        }
-    }
-});
 
 function renderFraction(swiper) {
 
-    let all = swiper.slides.length ;
+    let all = swiper.slides.length;
     let pos = swiper.realIndex + 1;
     let htmlPos = pos;
     let htmlAll = all;
